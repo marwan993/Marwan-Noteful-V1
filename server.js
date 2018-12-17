8d511138df449a71714118ef4334b49391a7595e
Marwan Alaloosi <manoalosi@gmail.com>
2018-12-17 16:21:08 -0600

    created Express app and static server

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 'use strict';
 
 // Load array of notes
-const data = require('./db/notes');
+const express = require("express");
+const data = require("./db/notes");
+const app = express();
+
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+
+app.use(express.static('public'));
+
+app.listen(8080, function() {
+    console.info(`Server litening on ${this.address().port}`);
+
+}).on('error', err =>{
+    console.error(err);
+})
+
+
+
