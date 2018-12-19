b604d2333de7a0878224c0fa4e98c7cf7f572302
Marwan Alaloosi <manoalosi@gmail.com>
2018-12-18 18:47:53 -0600

    tested and worked

--- middleware/logger.js
+++ middleware/logger.js
@@ -3,7 +3,8 @@ function logger(req, res, next) {
     const now = new Date();
     console.log(
         `${now.toLocaleDateString()} ${req.method} ${req.url}`);
-
     next()
 
-}
+    };
+
+    module.exports = logger;
--- server.js
+++ server.js
@@ -4,32 +4,31 @@
 const express = require("express");
 const data = require("./db/notes");
 const simDB = require("./db/simDB");
-// const logger = require("./middleware/logger");
 const notes = simDB.initialize(data);
-const app = express();
+const logger = require("./middleware/logger");
 const config = require("./config");
 const {PORT} = config;
+
+const app = express()
 
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
-// app.use(logger);
+app.use(logger);
 app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/api/notes', (req, res, next) => {
-    console.log(req.query);
-    const {searchTerm} = req.query;
-    notes.filter({}, (err,list) => {
-        if(err){
-            return next(err);
-        }
-        res.json(list.filter(item => item.title.includes(searchTerm))); 
+    const { searchTerm } = req.query;
+  
+    notes.filter(searchTerm, (err, list) => {
+      if (err) {
+        return next(err);
+      }
+      res.json(list);
     });
-    
   });
-
 app.get('/api/notes/:id', (req,res) => {
     const id = req.params.id;
     notes.find(id,(err,item) => {
@@ -39,50 +38,39 @@ app.get('/api/notes/:id', (req,res) => {
         if(item){
             res.json(item);
         } else {
-            console.log('not found');
-        }
-    });
-});
-
-app.get('/api/notes/id', (req, res)=>{
-    const id = req.params.id;
-    notes.find(id, (err, item) => {
-        if(err){
-            return next(err);
-        }
-        if(item){
-            res.json(item);
-        } else {
-            console.log('not found')
+            next()
         }
     });
 });
 
 app.put('/api/notes/:id', (req, res, next) => {
     const id = req.params.id;
+  
     const updateObj = {};
-    const updateFields = ['title', 'content'];
-
-    updateFields.forEach(field => {
-        if(field in req.body) {
-            updateObj[field] = req.body[field];
-        }
+    const updateableFields = ['title', 'content'];
+  
+    updateableFields.forEach(field => {
+      if (field in req.body) {
+        updateObj[field] = req.body[field];
+      }
     });
+  
 
-
+  
     notes.update(id, updateObj, (err, item) => {
-        if(err){
-            return next(err);
-        }
-        if(item){
-            res.json(item);
-        } else {
-            next();
-        }
+      if (err) {
+        return next(err);
+      }
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
     });
+  });
+  
 
 
-});
 
 
 app.use(function(req, res, next){
